49f658e5d8ff0a2794346f57d671f3bfed958786
fatima <hoory1020@gmail.com>
2020-02-26 11:23:01 +0200

    first commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/fatima/Desktop/intergrify/Integrify training/gatsby/jamstack-typescript-boilerplate/.cache/dev-404-page.js"))),
+  "component---src-pages-404-index-tsx": hot(preferDefault(require("/home/fatima/Desktop/intergrify/Integrify training/gatsby/jamstack-typescript-boilerplate/src/pages/404/index.tsx"))),
+  "component---src-pages-index-tsx": hot(preferDefault(require("/home/fatima/Desktop/intergrify/Integrify training/gatsby/jamstack-typescript-boilerplate/src/pages/index.tsx"))),
+  "component---src-pages-page-2-index-tsx": hot(preferDefault(require("/home/fatima/Desktop/intergrify/Integrify training/gatsby/jamstack-typescript-boilerplate/src/pages/page-2/index.tsx")))
+}
+
